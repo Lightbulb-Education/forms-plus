595887a302ef5e4e6171926d7a2239efb422e689
jchacko11 <jonathan.j.chacko@gmail.com>
2020-05-15 20:08:49 -0500

    Bulma delete button, css disable clicks, random hidden value, cleaning up

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,66 +1,70 @@
 $(document).ready(function () {
-    //configuration for radio buttons
-    $(".freebirdFormviewerViewItemsRadioChoicesContainer").each(function () {
-        //clone last option to create hidden element
-        var clone = $(this).children().last().clone().appendTo(this)
-        clone.attr("aria-hidden", "true")
-        clone.find(".appsMaterialWizToggleRadiogroupEl")
-            .attr("label", "Hidden Option")
-            .attr("data-value", "sdfasdasdfasdf")
-        clone.hide()
+    if (window.location.href.includes("https://docs.google.com/forms/d/e/")) {
+        //configuration for radio buttons
 
-        //add a eliminate button to each option
-        $(this).find(".freebirdFormviewerViewItemsRadioOptionContainer").each(function () {
-            $(this).append("<button class='x-button'>✖</button>").addClass("relative-position")
+        //random hidden option value
+        var r = Math.random().toString(36).substring(15);
+
+        $(".freebirdFormviewerViewItemsRadioChoicesContainer").each(function () {
+            //clone last option to create hidden element
+            var clone = $(this).children().last().clone().appendTo(this)
+            clone.attr("aria-hidden", "true")
+            clone.find(".appsMaterialWizToggleRadiogroupEl")
+                .attr("label", "Hidden Option")
+                .attr("data-value", r)
+            clone.hide()
+
+            //add a eliminate button to each option
+            $(this).find(".freebirdFormviewerViewItemsRadioOptionContainer").each(function () {
+                $(this).append("<a class='x-button'></a>")
+            })
+
         })
 
-    })
+        //configuration for checkboxes
+        $(".freebirdFormviewerViewItemsCheckboxOptionContainer").each(function () {
+            //add a eliminate button to each option
+            $(this).find(".freebirdFormviewerViewItemsCheckboxChoice").each(function () {
+                $(this).append("<a class='x-button'></a>")
+            })
 
-    //configuration for checkboxes
-    $(".freebirdFormviewerViewItemsCheckboxOptionContainer").each(function () {
-        //add a eliminate button to each option
-        $(this).find(".freebirdFormviewerViewItemsCheckboxChoice").each(function () {
-            $(this).append("<button class='x-button'>✖</button>").addClass("relative-position")
         })
 
-    })
 
+        $(".freebirdThemedRadio").click(function () {
+            var allOptions = $(this).closest(".freebirdFormviewerViewItemsRadioChoicesContainer").children()
 
-    $(".freebirdThemedRadio").click(function () {
-        var allOptions = $(this).closest(".freebirdFormviewerViewItemsRadioChoicesContainer").children()
+            //if radio is already checked...
+            if ($(this).children().hasClass("isChecked")) {
+                //...select the hidden option instead
+                allOptions.last().children("label").click()
+            }
+        })
 
-        //if radio is already checked...
-        if ($(this).children().hasClass("isChecked")) {
-            //...select the hidden option instead
-            allOptions.last().children("label").click()
+        //eliminate option on x-button click
+        $(".x-button").click(eliminateOption)
+
+        function eliminateOption() {
+            var parent = $(this).parent()
+
+            //if already eliminated, undo
+            //else eliminate
+            if (parent.attr("data-disabled") == "true") {
+                //enable clicks to button
+                parent.children("label").removeClass("no-click")
+
+                parent.removeClass("disabled-option")
+                parent.attr("data-disabled", "false")
+
+            } else {
+                //disable clicks to button
+                parent.children("label").addClass("no-click")
+
+                parent.addClass("disabled-option")
+                parent.attr("data-disabled", "true")
+            }
+
         }
-    })
-
-    //eliminate option on x-button click
-    $(".x-button").click(eliminateOption)
-
-    function eliminateOption() {
-        var parent = $(this).parent()
-
-        //if already eliminated, undo
-        //else eliminate
-        if (parent.attr("data-disabled") == "true") {
-            //enable clicks to button
-            parent.unbind('click');
-
-            parent.removeClass("disabled-option")
-            parent.attr("data-disabled", "false")
-
-        } else {
-            //disable clicks to button
-            parent.bind('click', function () {
-                return false;
-            });
-
-            parent.addClass("disabled-option")
-            parent.attr("data-disabled", "true")
-        }
-
     }
 });
 
